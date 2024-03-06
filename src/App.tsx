1e785ca15696b56697cdb88bd473af5edaa0223e
Xinxin He <xinxinhe1991@gmail.com>
2024-03-06 00:25:51 -0800

    Fix Amplify deployment issue

--- src/App.tsx
+++ src/App.tsx
@@ -3,7 +3,6 @@ import "./App.css";
 import { Navbar } from "./components/navbar/Navbar";
 import { MainView } from "./router/MainView";
 import { ModalView } from "./modals/ModalView";
-import "./styles/root.css";
 
 function App() {
   return (
