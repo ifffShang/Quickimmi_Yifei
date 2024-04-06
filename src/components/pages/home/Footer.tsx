85604cabf3503a28c8e729f8af15ac34eef61df6
Xinxin He <xinxinhe1991@gmail.com>
2024-04-05 22:15:38 -0700

    Update footer link

--- src/components/pages/home/Footer.tsx
+++ src/components/pages/home/Footer.tsx
@@ -42,16 +42,15 @@ function FooterLinks() {
               {t("AboutQuickimmi")}
             </QLink>
           </li>
-          <li>
-            <QLink color="white">{t("Pricing")}</QLink>
-          </li>
         </ul>
       </div>
       <div>
         <h3>{t("Compare")}</h3>
         <ul>
           <li>
-            <QLink color="white">{t("ImmigrationWebsite")}</QLink>
+            <QLink color="white" onClick={() => open("https://www.uscis.gov")}>
+              {t("ImmigrationWebsite")}
+            </QLink>
           </li>
         </ul>
       </div>
