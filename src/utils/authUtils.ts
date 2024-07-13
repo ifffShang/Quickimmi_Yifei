edbe538f6883df8f57738dc0cc3f88bb65c88aaa
Brandon(Bin) Qi <binq@amazon.com>
2024-07-12 22:58:25 -0400

    Upgradge aws-amplify version to fix the refresh token not returning token issue.

--- src/utils/authUtils.ts
+++ src/utils/authUtils.ts
@@ -4,6 +4,7 @@ import { resetAuthState, updateAuthState } from "../reducers/authSlice";
 import { closeModal, openModal } from "../reducers/commonSlice";
 import { fetchAuthSession } from "aws-amplify/auth";
 import { InMemoryCache } from "../cache/inMemoryCache";
+import {message} from "antd";
 
 export const signOutCurrentUser = (dispatch: AppDispatch) => {
   signOut().then(() => {
@@ -22,8 +23,15 @@ export const startTokenExpirationTimer = (dispatch: AppDispatch) => {
 };
 
 export const refreshToken = async (dispatch: AppDispatch) => {
+  /*
+   * The fetchAuthSession API automatically refreshes the user's session
+   * when the authentication tokens have expired and a valid refreshToken is present.
+   * https://docs.amplify.aws/gen1/javascript/build-a-backend/auth/manage-user-session/#refreshing-sessions
+   */
   const { tokens } = await fetchAuthSession({ forceRefresh: true });
+  console.log("tokens", tokens);
   if (!tokens || !tokens.accessToken) {
+    message.error("Empty tokens in session after refresh");
     throw new Error("Empty tokens in session after refresh");
   }
   const accessToken = tokens.accessToken.toString();
